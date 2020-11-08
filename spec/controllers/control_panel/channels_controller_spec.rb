7b0a253d64dc8e3cf6ef708179be018798cb191f
Max Shelley <max@measuredbrilliance.com>
2020-11-08 11:14:43 +0000

    WIP: Use a user's organisation to determine access to CP channels controller

--- spec/controllers/control_panel/channels_controller_spec.rb
+++ spec/controllers/control_panel/channels_controller_spec.rb
@@ -6,10 +6,14 @@ describe ControlPanel::ChannelsController, type: :controller do
   let(:channel) { FactoryBot.create(:channel) }
 
   context '#show' do
-    context 'with a confirmed, signed-in user' do
-      before { sign_in FactoryBot.create(:user, :confirmed) }
+    context 'with a confirmed, signed-in user that belongs to the same organisation as the channel' do
+      let(:user) { FactoryBot.create(:user, :confirmed) }
+      let(:channel) { FactoryBot.create(:channel, organisation: user.organisation) }
+
+      before { sign_in user }
 
       it 'should return a successful response' do
+        sign_in user
         get :show, params: { id: channel.id }
         expect(response).to have_http_status(:ok)
       end
